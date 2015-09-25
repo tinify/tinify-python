e132cd3d8baf9605768fe1b8b7295c3a40783bfb
Rolf Timmermans <rolftimmermans@voormedia.com>
2015-09-25 10:43:54 +0200

    Move API key check.

--- test/integration.py
+++ test/integration.py
@@ -3,12 +3,12 @@ import sys, os
 if os.environ.get("TRAVIS_PULL_REQUEST") and os.environ.get("TRAVIS_PULL_REQUEST") != "false":
     sys.exit(0)
 
+if not os.environ.get("TINIFY_KEY"):
+    sys.exit("Set the TINIFY_KEY environment variable.")
+
 import tinify, unittest, tempfile
 
-class Client(unittest.TestCase):
-    if not os.environ.get("TINIFY_KEY"):
-        sys.exit("Set the TINIFY_KEY environment variable.")
-
+class ClientIntegrationTest(unittest.TestCase):
     tinify.key = os.environ.get("TINIFY_KEY")
 
     unoptimized_path = os.path.join(os.path.dirname(__file__), 'examples', 'voormedia.png')
