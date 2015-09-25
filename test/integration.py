f7b58bc0313b95aa9167e60dc1411a6860821a83
Rolf Timmermans <rolftimmermans@voormedia.com>
2015-09-25 10:15:19 +0200

    Integration tests.

---
+++ test/integration.py
@@ -0,0 +1,25 @@
+import sys, os
+
+if os.environ.get("TRAVIS_PULL_REQUEST") and os.environ.get("TRAVIS_PULL_REQUEST") != "false":
+    sys.exit(0)
+
+import tinify, unittest, tempfile
+
+class Client(unittest.TestCase):
+    if not os.environ.get("TINIFY_KEY"):
+        sys.exit("Set the TINIFY_KEY environment variable.")
+
+    tinify.key = os.environ.get("TINIFY_KEY")
+
+    unoptimized_path = os.path.join(os.path.dirname(__file__), 'examples', 'voormedia.png')
+    optimized = tinify.from_file(unoptimized_path)
+
+    def test_should_compress(self):
+        with tempfile.NamedTemporaryFile() as tmp:
+            self.optimized.to_file(tmp.name)
+            self.assertTrue(os.path.getsize(tmp.name) < 1500)
+
+    def test_should_resize(self):
+        with tempfile.NamedTemporaryFile() as tmp:
+            self.optimized.resize(method="fit", width=50, height=20).to_file(tmp.name)
+            self.assertTrue(os.path.getsize(tmp.name) < 800)
