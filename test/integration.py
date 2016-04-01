2d19ee1f769aab78b981a335b7705819735047c4
Rolf Timmermans <rolftimmermans@voormedia.com>
2016-04-01 10:36:11 +0200

    Add integration test for metadata preservation.

--- test/integration.py
+++ test/integration.py
@@ -9,21 +9,57 @@ class ClientIntegrationTest(unittest.TestCase):
     tinify.key = os.environ.get("TINIFY_KEY")
 
     unoptimized_path = os.path.join(os.path.dirname(__file__), 'examples', 'voormedia.png')
+    optimized = tinify.from_file(unoptimized_path)
 
     def test_should_compress_from_file(self):
-        source = tinify.from_file(self.unoptimized_path)
         with tempfile.NamedTemporaryFile() as tmp:
-            source.to_file(tmp.name)
-            self.assertTrue(0 < os.path.getsize(tmp.name) < 1500)
+            self.optimized.to_file(tmp.name)
+
+            size = os.path.getsize(tmp.name)
+            contents = tmp.read()
+
+            self.assertTrue(1000 < size < 1500)
+
+            # width == 137
+            self.assertIn(b'\x00\x00\x00\x89', contents)
+            self.assertNotIn(b'Copyright Voormedia', contents)
 
     def test_should_compress_from_url(self):
         source = tinify.from_url('https://raw.githubusercontent.com/tinify/tinify-python/master/test/examples/voormedia.png')
         with tempfile.NamedTemporaryFile() as tmp:
             source.to_file(tmp.name)
-            self.assertTrue(0 < os.path.getsize(tmp.name) < 1500)
+
+            size = os.path.getsize(tmp.name)
+            contents = tmp.read()
+
+            self.assertTrue(1000 < size < 1500)
+
+            # width == 137
+            self.assertIn(b'\x00\x00\x00\x89', contents)
+            self.assertNotIn(b'Copyright Voormedia', contents)
 
     def test_should_resize(self):
-        source = tinify.from_file(self.unoptimized_path)
         with tempfile.NamedTemporaryFile() as tmp:
-            source.resize(method="fit", width=50, height=20).to_file(tmp.name)
-            self.assertTrue(0 < os.path.getsize(tmp.name) < 1000)
+            self.optimized.resize(method="fit", width=50, height=20).to_file(tmp.name)
+
+            size = os.path.getsize(tmp.name)
+            contents = tmp.read()
+
+            self.assertTrue(500 < size < 1000)
+
+            # width == 50
+            self.assertIn(b'\x00\x00\x00\x32', contents)
+            self.assertNotIn(b'Copyright Voormedia', contents)
+
+    def test_should_preserve_metadata(self):
+        with tempfile.NamedTemporaryFile() as tmp:
+            self.optimized.preserve("copyright", "creation").to_file(tmp.name)
+
+            size = os.path.getsize(tmp.name)
+            contents = tmp.read()
+
+            self.assertTrue(1000 < size < 2000)
+
+            # width == 137
+            self.assertIn(b'\x00\x00\x00\x89', contents)
+            self.assertIn(b'Copyright Voormedia', contents)
