f10fa7e3088141deb507eccd8708254befd9aff1
Timothy Robinson <robinsti@engr.orst.edu>
2016-04-10 19:03:40 -0700

    Update result.py

--- tinify/result.py
+++ tinify/result.py
@@ -2,23 +2,17 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from . import ResultMeta
-<<<<<<< HEAD
 from google.appengine.ext import ndb
-=======
->>>>>>> 2d19ee1f769aab78b981a335b7705819735047c4
 
 class Result(ResultMeta):
     def __init__(self, meta, data):
         ResultMeta.__init__(self, meta)
         self.data = data
 
-<<<<<<< HEAD
     def to_blob(self, key):
         img_store = key.get()
         img_store.img = self.data
 
-=======
->>>>>>> 2d19ee1f769aab78b981a335b7705819735047c4
     def to_file(self, path):
         if hasattr(path, 'write'):
             path.write(self.data)
