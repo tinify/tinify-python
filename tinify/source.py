2dfbcc753b7423ae8cfa36a00221dce06cdc4f86
Dax Mickelson <dmickels@cisco.com>
2020-01-09 09:00:30 -0700

    Clean up imports

--- tinify/source.py
+++ tinify/source.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
-
 import tinify
-from . import Result, ResultMeta
+from .result import Result
+from .result_meta import ResultMeta
+
 
 class Source(object):
     @classmethod
