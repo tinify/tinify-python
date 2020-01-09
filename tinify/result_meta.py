61fc45be6891e111e1f41552debc37b21ecb368a
Dax Mickelson <dmickels@cisco.com>
2020-01-09 09:00:48 -0700

    PEP8

--- tinify/result_meta.py
+++ tinify/result_meta.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+
 class ResultMeta(object):
     def __init__(self, meta):
         self._meta = meta
